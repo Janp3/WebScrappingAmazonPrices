52b3efc31aee9a117cd172a01d2607480d52b029
Jean <Jean.c.drawing@gmail.com>
2023-01-19 12:47:38 -0300

    full commit

--- webscrapping.py
+++ webscrapping.py
@@ -35,8 +35,7 @@ path_price = driver.find_elements(By.XPATH, '//span[@class="a-price-whole"]')
 
 main_element = driver.find_elements(
     By.XPATH,
-    '//div[@class="a-section a-spacing-small puis-padding-left-' +
-    'small puis-padding-right-small"]'
+    '//div[@class="a-section a-spacing-base"]'
 )
 
 # iterating through all the containers that has the title,
@@ -47,9 +46,8 @@ for element in main_element:
         './/span[@class="a-size-base-plus a-color-base a-text-normal"]'
     ).text
     price = element.find_element(
-        By.CLASS_NAME,
-        'a-price-whole'
-        # './/span[@class="a-price-whole"]'
+        By.XPATH,
+        '//span[@class="a-price-whole"]'
     ).text
     page.append([title, f"R$ {price}"])
 
